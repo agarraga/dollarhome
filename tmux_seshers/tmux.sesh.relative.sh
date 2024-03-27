d3ad6d72b89cd6dbde152cda254c4c3b48a44d02
agarraga <83713047+agarraga@users.noreply.github.com>
2024-03-27 20:32:33 +0000

    [tmux] hack proper cd to working dir in seshers

--- tmux_seshers/tmux.sesh.relative.sh
+++ tmux_seshers/tmux.sesh.relative.sh
@@ -2,13 +2,14 @@
 
 sesh="relative"
 win0="editing"
+wd="~/repos/relativecpp"
 
-tmux new-session -s $sesh -c '~/repos/relativecpp' -n $win0 -d
+tmux new-session -s $sesh -c $wd -n $win0 -d
 
 tmux split-window -h -t $sesh:$win0
 tmux resize-pane -x 86 -t $sesh:$win0.left
-tmux send-keys -t $sesh:$win0.left 'nvim' C-m
-tmux send-keys -t $sesh:$win0.right 'tree' C-m
+tmux send-keys -t $sesh:$win0.left "cd $wd; nvim" C-m
+tmux send-keys -t $sesh:$win0.right "cd $wd; tree" C-m
 
 tmux select-pane -t $sesh:$win0.left
-tmux attach -t $sesh
+tmux attach-session -t $sesh
--- tmux_seshers/tmux.sesh.stereo.sh
+++ tmux_seshers/tmux.sesh.stereo.sh
@@ -2,13 +2,16 @@
 
 sesh="stereo"
 win0="library|player"
+wd="~/Desktop"
 
-tmux new-session -s $sesh -c '~/Desktop' -n $win0 -d
+tmux new-session -s $sesh -c $wd -n $win0 -d
 
 tmux split-window -h -t $sesh:$win0
 tmux resize-pane -x 64 -t $sesh:$win0.right
-tmux send-keys -t $sesh:$win0.left 'cd music-limbo; tree' C-m
-tmux send-keys -t $sesh:$win0.right 'cmus' C-m
+tmux send-keys -t $sesh:$win0.left "cd $wd; tree" C-m
+tmux send-keys -t $sesh:$win0.right "cd $wd; cmus" C-m
 
 tmux select-pane -t $sesh:$win0.right
 tmux attach -t $sesh
+
+# The $wd in new session doesn't seem to work????
