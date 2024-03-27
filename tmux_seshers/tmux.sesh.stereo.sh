be4bc47daaa34c7f06c87c38222a16b5d939f627
agarraga <83713047+agarraga@users.noreply.github.com>
2024-03-27 22:04:01 +0000

    [tmux] better tree for tmux.sesh.stereo

--- tmux_seshers/tmux.sesh.stereo.sh
+++ tmux_seshers/tmux.sesh.stereo.sh
@@ -8,10 +8,10 @@ tmux new-session -s $sesh -c $wd -n $win0 -d
 
 tmux split-window -h -t $sesh:$win0
 tmux resize-pane -x 64 -t $sesh:$win0.right
-tmux send-keys -t $sesh:$win0.left "cd $wd; tree" C-m
+tmux send-keys -t $sesh:$win0.left "cd $wd; tree -L 2" C-m
 tmux send-keys -t $sesh:$win0.right "cd $wd; cmus" C-m
 
 tmux select-pane -t $sesh:$win0.right
 tmux attach -t $sesh
 
-# The $wd in new session doesn't seem to work????
+# The `-c $wd' in new session doesn't seem to work????
